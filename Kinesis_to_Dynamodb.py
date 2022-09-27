c72f3b800ccc5f9be6b7a07f2bc7c830c2106a53
shruthi2611 <79125978+shruthi2611@users.noreply.github.com>
2022-09-27 14:11:48 -0400

    update

--- Kinesis_to_Dynamodb.py
+++ Kinesis_to_Dynamodb.py
@@ -9,12 +9,12 @@ def lambda_handler(event, context):
     client = boto3.client('dynamodb')
     for record in event['Records']:
         t_record = base64.b64decode(record['kinesis']['data'])
-        str_record = str(t_record,'utf-8')
-        dict_record = json.loads(str_record)
-        customer_key = dict()
-        customer_key.update({'CustomerID': {"N": str(dict_record['CustomerID'])}})
-        ex_customer = dict()
-        ex_customer.update({str(dict_record['InvoiceNo']): {'Value':{"S":'Some overview JSON for the UI goes here'},"Action":"PUT"}})
+        s_record = str(t_record,'utf-8')
+        dict_record = json.loads(_record)
+        cust_key = dict()
+        cust_key.update({'CustomerID': {"N": str(dict_record['CustomerID'])}})
+        ex_cust = dict()
+        ex_cust.update({str(dict_record['InvoiceNo']): {'Value':{"S":'Some overview JSON for the UI goes here'},"Action":"PUT"}})
 
         response = client.update_item(TableName='Customer', Key = customer_key, AttributeUpdates = ex_customer)
         inventory_key = dict()
